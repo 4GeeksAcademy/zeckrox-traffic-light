9a74500c08d22afce50abd5b398a846856607e92
Stefano <tefanoboschetti@gmail.com>
2023-08-26 13:45:49 +0000

    Codigo mejorado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,16 +4,13 @@ import { useState } from "react";
 //create your first component
 const TrafficLight = () => {
 	const [activeLight, setActiveLight] = useState("red")
-	const redActive = ()=>{return activeLight=="red"?" active-red":""}
-	const yellowActive = ()=>{return activeLight=="yellow"?" active-yellow":""}
-	const greenActive = ()=>{return activeLight=="green"?" active-green":""}
 
 	return <>
 	<div className="traffic-pole"></div>
 	<div className="traffic-light">
-	<div onClick={()=>setActiveLight("red")} className={"light light-red" + redActive()}></div>
-	<div onClick={()=>setActiveLight("yellow")} className={"light light-yellow"+ yellowActive()}></div>
-	<div onClick={()=>setActiveLight("green")} className={"light light-green"+ greenActive()}></div>
+	<div onClick={()=>setActiveLight("red")} className={`light light-red ${activeLight=="red"? "active-red" : ""}`}></div>
+	<div onClick={()=>setActiveLight("yellow")} className={`light light-yellow ${activeLight=="yellow"? "active-yellow" : ""}`}></div>
+	<div onClick={()=>setActiveLight("green")} className={`light light-green ${activeLight=="green"? "active-green" : ""}`}></div>
 	</div>
 	</>
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,11 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import { useState } from "react";
-
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import TrafficLight from "./component/home.jsx";
-
 
 
 //render your react application
